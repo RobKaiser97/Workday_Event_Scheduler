d27dc6459289e865824937cf081c66acb88ee042
Robert Kaiser <paintballmanrob@gmail.com>
2023-08-01 06:03:10 -0400

    added click event for save button: local storage

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,12 +2,24 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
+// local storage. HINT: What does `this` reference in the click listener
+// function? How can DOM traversal be used to get the "hour-x" id of the
+// time-block containing the button that was clicked? How might the id be
+// useful when saving the description in local storage?
+$(".saveBtn").on("click", function () {
+  let timeSlot = $(this).parent().attr("id");
+  let task = $(this).siblings(".description").val().trim();
+  localStorage.setItem(timeSlot, task);
+});
+// Get tasks from local storage and add to the timeSlot
+$(function () {
+  $(".time-block").each(function () {
+    let timeSlot = $(this).attr("id");
+    $(this).children(".description").val(localStorage.getItem(timeSlot));
+  });
+});
 
 
 // TODO: Add code to apply the past, present, or future class to each time
